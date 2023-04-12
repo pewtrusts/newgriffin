5e6b628387ba9be05c1ce9bdcd746f62adbd55ed
Drew <aswinburne@pewtrusts.org>
2023-04-12 13:28:48 -0400

    update defaults for coloraxis

--- src/scripts/return-datalabel-formatter.js
+++ src/scripts/return-datalabel-formatter.js
@@ -6,7 +6,6 @@ export default function _returnDataLabelFormatter(obj) {
     var numberFormatter = returnNumberFormatter(numberFormat, 'datalabel', decimals);
 
     return function() {
-        console.log(this)
         return `<div class="datalabel">${this.point.id}<br> <span class="mobile-hide">${numberFormatter.call(this.point.value)}</span></div>`
     }
 }
